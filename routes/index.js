2b65d525746e48921eaed8a1cc2e226b2b6fc6d8
nur mizwari <nurmizwari04@gmail.com>
2022-07-21 15:33:22 +0700

    session login udah bener

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = require('express').Router()
 
 
 
-router.get("/",UserController.home)
+
 router.get("/register", UserController.registerForm)
 router.post("/register", UserController.postRegister)
 
@@ -17,10 +17,19 @@ router.post("/login",UserController.loginForm)
 
 router.use(function(req, res, next){
     console.log(req.session)
-    console.log('Time:', Date.now())
-    next()
+    // console.log('Time:', Date.now())
+    // next()
+
+    if(!req.session.userId){
+        const error = 'Please Login First'
+        res.redirect(`/login?error=${error}`)
+    }else{
+        next()
+        // kalau ada session bebas mau masuk kemana
+    }
   })
 
+  router.get("/",UserController.home)
   // route home
 
 
