e78b95736834853b14fa2235a17b8d8255363fa7
muhgifariakbar <muhgifariakbar@gmail.com>
2022-07-21 14:50:03 +0700

    Nodemailer Done

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,5 @@ router.get("/register", UserController.registerForm)
 router.post("/register", UserController.postRegister)
 router.get("/login", UserController.loginForm)
 router.get("/home/:productId/delete", UserController.postDelete)
-router.get("/add", UserController.postDelete)
 
 module.exports = router
