b06be76a39a72052a3eb9ec003c58e2556d81918
nur mizwari <nurmizwari04@gmail.com>
2022-07-21 13:27:59 +0700

    compare paswword 1

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,8 +1,13 @@
 const { Store,User } = require('../models');
+const bcrypt = require('bcryptjs')
 
 
 
 class UserController{
+
+    static home(req, res){
+        res.render('home')
+    }
 
     static registerForm(req, res){
         Store.findAll().then((result) => {  
@@ -14,18 +19,48 @@ class UserController{
     }
     static postRegister(req, res){
         // cretae user baru yang isinya username password role
-        const {email,password,role,userName} = req.body
-        // console.log(req.body);
+        const {email,password,role,StoreId,userName} = req.body
+        console.log(req.body);
         // res.send(req.body)
-        User.create({email,password,role,userName})
+        User.create({email,password,role,StoreId,userName})
         .then((newUser) => {
             res.redirect('/login')
         }).catch((err) => {
             res.send(err)
         });       
     }
-    static loginForm(req, res){
+
+    
+    static getForm(req, res){       
         res.render('login')
+        
+    }
+
+    static loginForm(req, res){
+        // apakah username sama password    yang diinput itu username nya ada?
+        // 1.findOne user dari username
+        //2 kalo user ada compare plain password apakah sama dengan hash password di db
+        // 3 kalo gak sama passwordnya gak boleh masuk ke home , keluar error
+         // 4 kalo pw sesuai maaka redirect ke home
+
+         const {userName, password} = req.body
+         User.findOne({where:{userName}})          
+         .then(user => { 
+            if (user) {
+                const isValidPassword = bcrypt.compareSync(password, user.password) // true or false
+                console.log(isValidPassword,'<<<<');
+                if (isValidPassword) {
+                    console.log(isValidPassword);
+                    return res.redirect('/')
+                } else{
+                    const error = 'invalid username or password'
+                    console.log(error);
+                    return res.redirect('/login')
+                }
+            }            
+         }).catch((err) => {
+            res.send(err)
+         });
     }
 }
 module.exports = UserController
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const UserController = require('../controller/UserController')
 const router = require('express').Router()
 
 
+router.get("/",UserController.home)
 router.get("/register", UserController.registerForm)
 router.post("/register", UserController.postRegister)
-router.get("/login",UserController.loginForm)
+router.get("/login",UserController.getForm)
+router.post("/login",UserController.loginForm)
 
 module.exports = router
