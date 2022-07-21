a139190c41af668fef9bfdd3391d008dddd730c8
muhgifariakbar <muhgifariakbar@gmail.com>
2022-07-21 12:32:23 +0700

    navbar

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get("/register", UserController.registerForm)
 router.post("/register", UserController.postRegister)
 router.get("/login", UserController.loginForm)
 router.get("/home/:productId/delete", UserController.postDelete)
+router.get("/add", UserController.postDelete)
 
 module.exports = router
